ae22325984f88d8de722aaf25f16dbc97e22497a
WangDongDong1234 <827495316@qq.com>
2022-01-07 00:17:00 +0800

    220107:第二集学习完成 1.xml获取bean 2.注解获取bean 3.bean的命名

---
+++ annotationSpecial01/src/main/java/com/atguigu/config/MainConfig.java
@@ -0,0 +1,15 @@
+package com.atguigu.config;
+
+import com.atguigu.bean.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//配置类==配置文件
+@Configuration  //告诉Spring这是一个配置类
+public class MainConfig {
+    //给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
+    @Bean("person99")
+    public Person person01(){
+        return new Person("李梦莎", 20);
+    }
+}
