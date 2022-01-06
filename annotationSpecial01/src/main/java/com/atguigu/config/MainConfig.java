2f70b343e8c9e148bc09c371407573c7350f5c5a
WangDongDong1234 <827495316@qq.com>
2022-01-07 01:08:43 +0800

    //excludeFilters = Filter[] ：指定扫描的时候按照什么规则排除那些组件 //includeFilters = Filter[] ：指定扫描的时候只需要包含哪些组件（必须禁用默认的扫描规则）

--- annotationSpecial01/src/main/java/com/atguigu/config/MainConfig.java
+++ annotationSpecial01/src/main/java/com/atguigu/config/MainConfig.java
@@ -4,10 +4,15 @@ import com.atguigu.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 //配置类==配置文件
 @Configuration  //告诉Spring这是一个配置类
-@ComponentScan(value = "com.atguigu")
+@ComponentScan(value = "com.atguigu",includeFilters ={
+    @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class, Service.class})
+},useDefaultFilters = false)
 public class MainConfig {
     //给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
     @Bean("person_zhujie")
