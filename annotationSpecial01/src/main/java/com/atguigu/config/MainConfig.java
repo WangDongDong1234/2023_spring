183b795cd9ed5c3bda79c87becfa429cff0fbbb8
WangDongDong1234 <827495316@qq.com>
2022-01-09 17:46:06 +0800

    03、组件注册-@ComponentScan-自动扫描组件&指定扫描规则(xml中的包扫描和注解中的包扫描)-学习完毕

--- annotationSpecial01/src/main/java/com/atguigu/config/MainConfig.java
+++ annotationSpecial01/src/main/java/com/atguigu/config/MainConfig.java
@@ -1,18 +1,17 @@
 package com.atguigu.config;
 
 import com.atguigu.bean.Person;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 //配置类==配置文件
 @Configuration  //告诉Spring这是一个配置类
-@ComponentScan(value = "com.atguigu",includeFilters ={
-    @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class, Service.class})
-},useDefaultFilters = false)
+@ComponentScans(value = {
+        @ComponentScan(value = "com.atguigu",includeFilters ={
+                @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class, Service.class})
+        },useDefaultFilters = false)
+})
 public class MainConfig {
     //给容器中注册一个Bean;类型为返回值的类型，id默认是用方法名作为id
     @Bean("person_zhujie")
